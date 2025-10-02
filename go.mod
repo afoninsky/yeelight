753ecf7be8069294ec9bf501dc131ae1dcc1c9ee
Anderey Afoninsky <andrey.afoninsky@booking.com>
2025-10-02 21:25:53 +0200

    refactor: change module path from mcp-yeelight to yeelight

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/afoninsky/mcp-yeelight
+module github.com/afoninsky/yeelight
 
 go 1.24.3
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/afoninsky/mcp-yeelight/yeelight"
+	"github.com/afoninsky/yeelight/yeelight"
 )
 
 var (
